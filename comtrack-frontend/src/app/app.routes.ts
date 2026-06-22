00326398b56627b139d712f447f3e5df05ca7854
safaacht <safaachtaoui@gmail.com>
2026-06-22 14:41:21 +0100

    Refactor routing configuration to use updated component imports and correct redirect path

--- comtrack-frontend/src/app/app.routes.ts
+++ comtrack-frontend/src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 
 
-import { LoginComponent } from './pages/LoginComponent/login.component';
-import { DashboardComponent } from './pages/DashbordComponent/dashboard.component';
-import { ClientsComponent } from './pages/ClientsComponent/clients.component';
-import { NavComponent } from './pages/NavComponent/nav.component';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Clients} from './pages/clients/clients';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login' , pathMatch: 'full'},
+    {path: '', redirectTo: 'app.index' , pathMatch: 'full'},
 
-    {path: 'login' , component: LoginComponent},
-    {path: 'dashboard' , component: DashboardComponent},
-    {path: 'clients' , component: ClientsComponent},
+    {path: 'login' , component: Login},
+    {path: 'signup', component: Signup},
+    {path: 'dashboard' , component: Dashboard},
+    {path: 'clients' , component: Clients},
 ];
