cc7205f28b1e9120ff4c1c97bf000e13ca294a40
safaacht <safaachtaoui@gmail.com>
2026-07-15 11:36:21 +0100

    Make footer component standalone

--- comtrack-frontend/src/app/shared/footer/footer.ts
+++ comtrack-frontend/src/app/shared/footer/footer.ts
@@ -4,6 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-footer',
   imports: [RouterLink],
+  standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
