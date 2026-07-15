fe768b6f151f339dd45da3c94e53f6c17eb7ebe6
safaacht <safaachtaoui@gmail.com>
2026-07-15 12:04:30 +0100

    feat(dashboard): add validation, regex, and error messages to modals and fix footer imports

--- comtrack-frontend/src/app/shared/footer/footer.ts
+++ comtrack-frontend/src/app/shared/footer/footer.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [],
   standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.css',
