8410612b999318e672db2c19d50eafd576dbdf38
safaacht <safaachtaoui@gmail.com>
2026-07-15 16:01:28 +0100

    remove duplicate column annotations for prenom and email

--- comtrack-backend/src/main/java/com/comtrack/entity/Commercial.java
+++ comtrack-backend/src/main/java/com/comtrack/entity/Commercial.java
@@ -16,12 +16,8 @@ public class Commercial {
     @Column(nullable=false)
     private String nom;
 
-    @Column(nullable=false , unique= true)
-    @Column(nullable=false , unique= true)
+    @Column(nullable=false )
     private String prenom;
-
-    @Column(nullable=false, unique=true)
-    private String email;
 
     @Column(nullable=false , unique=true)
     private String phone;
