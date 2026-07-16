4c3fcdb6a498770e2b759a41362b6c6c6b6a253b
safaacht <safaachtaoui@gmail.com>
2026-07-16 12:14:38 +0100

    Remove unique constraint from password column

--- comtrack-backend/src/main/java/com/comtrack/entity/User.java
+++ comtrack-backend/src/main/java/com/comtrack/entity/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(nullable=false , unique=true)
     private String email;
 
-    @Column(nullable=false , unique=true)
+    @Column(nullable=false)
     private String password;
 
     @Column(nullable=false)
