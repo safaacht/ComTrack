53651f0a709b1a5a28f96641f75a13b0f60bf940
safaacht <safaachtaoui@gmail.com>
2026-07-16 12:23:42 +0100

    Add LoginResponse class to handle authentication token

---
+++ comtrack-backend/src/main/java/com/comtrack/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.comtrack.dto;
+
+public class LoginResponse{
+    private String token;
+
+    public LoginResponse(){}
+
+    public LoginResponse(String token){
+        this.token=token
+    }
+
+    public String getToken() {return token;}
+
+    public void setToken(String token) {this.token = token;}
+    
+}
