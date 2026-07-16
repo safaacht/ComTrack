ca17f361b88f806291700d2bea434af2ea3f62e2
safaacht <safaachtaoui@gmail.com>
2026-07-16 12:14:55 +0100

    Add methods to find user by email and check email existence in UserRepository

--- comtrack-backend/src/main/java/com/comtrack/repository/UserRepository.java
+++ comtrack-backend/src/main/java/com/comtrack/repository/UserRepository.java
@@ -9,4 +9,8 @@ import  org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
     List<User> findByRole(Role role);
+    
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
